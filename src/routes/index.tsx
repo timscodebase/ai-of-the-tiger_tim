c2c5d4400f9770d1a3a37dba626227e929dfd131
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-09 18:22:37 -0800

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <main class="max-w-4xl mx-auto p-4">
+      <h1 class="text-6xl">Hi 👋</h1>
+    </main>
+  );
+});
