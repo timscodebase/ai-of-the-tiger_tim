cd69f9ee599d56c3135ba51fb73fbd1a35c3e1e5
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-01-11 17:29:18 -0800

    done

--- stages/5/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Dialog } from './Dialog.jsx'
-export { default as Input } from './Input.jsx'
-export { default as Svg } from './Svg.jsx'
--- stages/5/global.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Component as QC, HTMLAttributes as QH } from "@builder.io/qwik"
-
-declare global {
-  export type Component<T> = QC<T>
-  export type HTMLAttributes<T> = QH<T>
-}
